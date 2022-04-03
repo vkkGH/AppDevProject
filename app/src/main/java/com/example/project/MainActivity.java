5a8fc4c498c347f279c00810b28715a14d2024e8
vkkGH <vikkosv@protonmail.com>
2022-04-03 15:12:11 -0400

    commit

---
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signinbutton, signupbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signinbutton = findViewById(R.id.signinbutton);
+        signupbutton = findViewById(R.id.signupbutton);
+
+        signupbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new SignUpFragment()); }
+        }); {
+
+        }
+        signinbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replaceFragment(new LoginFragment()); }
+        }); {
+
+        }
+        getSupportActionBar().hide();
+        getWindow().setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.black));
+    }
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
